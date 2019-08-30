4f56096ed2a0130c7881d9c170fb35eb12d6174e
danielkang1003 <danielkang1003@gmail.com>
2019-08-30 15:31:12 +0900

    Updates

---
+++ precache-manifest.7f78f7b1ef273505d27e05ff96c477d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ba79cb3cc8f2c56e020042006fbdbb",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "456d632a24198632ec59",
+    "url": "/movie_app_2019/static/css/main.1f24cfb2.chunk.css"
+  },
+  {
+    "revision": "df73f757847819ea6385",
+    "url": "/movie_app_2019/static/js/2.0dc785e0.chunk.js"
+  },
+  {
+    "revision": "456d632a24198632ec59",
+    "url": "/movie_app_2019/static/js/main.cf443c25.chunk.js"
+  },
+  {
+    "revision": "30a14d7c554eb6584568",
+    "url": "/movie_app_2019/static/js/runtime~main.353f3f61.js"
+  }
+]);
